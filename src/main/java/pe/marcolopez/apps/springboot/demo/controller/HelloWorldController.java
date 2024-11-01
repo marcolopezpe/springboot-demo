15135e097c89f1bcee82277e4db4329ce05867f1
Marco López <marcolopezpe@outlook.com>
2024-10-31 22:43:01 -0500

    first commit

---
+++ src/main/java/pe/marcolopez/apps/springboot/demo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package pe.marcolopez.apps.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping
+  public String helloWorld() {
+    return "Hello World By Marco Lopez!";
+  }
+}
